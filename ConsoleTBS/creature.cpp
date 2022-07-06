f775b5a64e0d25cdc0e9a44b520d920bd6f67dc1
PanMaksim <pirrot.max@gmail.com>
2022-07-06 14:49:36 +0300

    Renaming in Creature.h and .cpp

--- ConsoleTBS/creature.cpp
+++ ConsoleTBS/creature.cpp
@@ -46,7 +46,7 @@ const std::string_view creature_database_second_name[]{
 	"Pestilens",
 };
 
-Creature::Creature(CreatureRace race, std::array<int, static_cast<int>(CreatureStatId::kCreatureStatMax)> target_creature_stats) : race_{ race } {
+Creature::Creature(CreatureRace creature_race, std::array<int, static_cast<int>(CreatureStatId::kCreatureStatMax)> target_creature_stats) : race_{ creature_race } {
 	std::array<CreatureStat, static_cast<int>(CreatureStatId::kCreatureStatMax)>::pointer
 		creature_stat_ptr{ creature_stats_.data() };
 
@@ -73,13 +73,13 @@ Creature::Creature(CreatureRace race, std::array<int, static_cast<int>(CreatureS
 	name_ = generate_name();
 }
 
-Creature::Creature(const Creature* creature) : race_{ creature->get_race() } {
-	std::copy(creature->creature_stats_.begin(), creature->creature_stats_.end(), this->creature_stats_.begin());
+Creature::Creature(const Creature* creature_ptr) : race_{ creature_ptr->get_race() } {
+	std::copy(creature_ptr->creature_stats_.begin(), creature_ptr->creature_stats_.end(), this->creature_stats_.begin());
 	name_ = generate_name();
 }
 
-CreatureStat Creature::get_certain_stat_current_and_max(CreatureStatId creature_stat) const { return creature_stats_[static_cast<int>(creature_stat)]; }
-int Creature::get_certain_stat_current_value(CreatureStatId creature_stat) const { return creature_stats_[static_cast<int>(creature_stat)].current; }
+CreatureStat Creature::get_certain_stat_current_and_max(CreatureStatId stat_id) const { return creature_stats_[static_cast<int>(stat_id)]; }
+int Creature::get_certain_stat_current_value(CreatureStatId stat_id) const { return creature_stats_[static_cast<int>(stat_id)].current; }
 CreatureRace Creature::get_race() const { return race_; }
 int Creature::get_army_id() const { return army_id_; }
 size_t Creature::get_creature_id() const { return creature_id_; }
@@ -93,9 +93,9 @@ void Creature::change_certain_stat_current_value(CreatureStatId stat_id, int cha
 void Creature::join_army(int army_id) { army_id_ = army_id; }
 
 
-int Creature::roll_stat_with_bonus(CreatureStatId creature_stat) const {
+int Creature::roll_stat_with_bonus(CreatureStatId stat_id) const {
 	return static_cast<int>(
-		this->get_certain_stat_current_value(creature_stat) * creature_database_get_roll_result_multiplier(
+		this->get_certain_stat_current_value(stat_id) * creature_database_get_roll_result_multiplier(
 			static_cast<RollResult>(get_random_number(static_cast<int>(RollResult::kRollResultMin), static_cast<int>(RollResult::kRollResultMax)))));
 }
 
@@ -105,16 +105,16 @@ const std::string Creature::generate_name() {
 }
 
 // should add multipliers for different ATK chances (Critical, Success, Small hit)
-int Creature::calculate_received_damage(const Creature* attacker, double multiplier) {
-	return static_cast<int>(attacker->get_certain_stat_current_value(CreatureStatId::kDAMAGE_ML_PHS) * multiplier
+int Creature::calculate_received_damage(const Creature* attacker_ptr, double damage_multiplier) {
+	return static_cast<int>(attacker_ptr->get_certain_stat_current_value(CreatureStatId::kDAMAGE_ML_PHS) * damage_multiplier
 			- this->get_certain_stat_current_value(CreatureStatId::kArmor_PHS));
 }
 
 void Creature::apply_stat_multiplier(CreatureStatMultiplier stat_multiplier) {
-	creature_stats_[static_cast<int>(stat_multiplier.stat_id)].current = static_cast<int>(static_cast<float>(creature_stats_[static_cast<int>(stat_multiplier.stat_id)].current) * stat_multiplier.multiplier);
+	creature_stats_[static_cast<int>(stat_multiplier.stat_id)].current = static_cast<int>(static_cast<float>(creature_stats_[static_cast<int>(stat_multiplier.stat_id)].max) * stat_multiplier.multiplier);
 }
 
 void Creature::delete_stat_multiplier(CreatureStatMultiplier stat_multiplier) {
-	creature_stats_[static_cast<int>(stat_multiplier.stat_id)].current = static_cast<int>(static_cast<float>(creature_stats_[static_cast<int>(stat_multiplier.stat_id)].current) / stat_multiplier.multiplier);
+	creature_stats_[static_cast<int>(stat_multiplier.stat_id)].current = static_cast<int>(static_cast<float>(creature_stats_[static_cast<int>(stat_multiplier.stat_id)].max) / stat_multiplier.multiplier);
 }
 
--- ConsoleTBS/creature.h
+++ ConsoleTBS/creature.h
@@ -18,8 +18,8 @@ static size_t creature_id_counter{};
 
 class Creature {
 public:
-	Creature(CreatureRace race, std::array<int, static_cast<int>(CreatureStatId::kCreatureStatMax)> target_creature_stats);
-	Creature(const Creature* creature);
+	Creature(CreatureRace creature_race, std::array<int, static_cast<int>(CreatureStatId::kCreatureStatMax)> target_creature_stats);
+	Creature(const Creature* creature_ptr);
 	//Creature(CreatureTemplate creature_template) : Creature(*database_create_creature_by_template(creature_template)) {}
 	
 	//bool operator==(const Creature& another_creature) {
@@ -29,8 +29,8 @@ public:
 
 	// can be realized by pointer to first element
 	//std::array<int, static_cast<int>(CreatureStatId::CreatureStatMax)>* get_all_stats() { return &creature_stats_; }
-	CreatureStat get_certain_stat_current_and_max(CreatureStatId creature_stat) const;
-	int get_certain_stat_current_value(CreatureStatId creature_stat) const;
+	CreatureStat get_certain_stat_current_and_max(CreatureStatId stat_id) const;
+	int get_certain_stat_current_value(CreatureStatId stat_id) const;
 	CreatureRace get_race() const;
 	int get_army_id() const;
 	size_t get_creature_id() const;
@@ -40,8 +40,8 @@ public:
 	void change_certain_stat_current_value(CreatureStatId stat_id, int change_amount);
 
 	void join_army(int army_id);
-	int roll_stat_with_bonus(CreatureStatId creature_stat) const; // returns stat after bonus multiplier
-	int calculate_received_damage(const Creature* attacker, double multiplier);
+	int roll_stat_with_bonus(CreatureStatId stat_id) const; // returns stat after bonus damage_multiplier
+	int calculate_received_damage(const Creature* attacker_ptr, double damage_multiplier);
 	
 	// in future should be created new class for effects and shoving active effects on screen
 	void apply_stat_multiplier(CreatureStatMultiplier stat_multiplier);
@@ -59,7 +59,7 @@ private:
 
 	// std::vector<Effect> active_effects;
 
-	const std::string generate_name(); // in future should use race and culture
+	const std::string generate_name(); // in future should use creature_race and culture
 };
 
 const Creature* creature_database_get_templates(CreatureTemplate creature_template);
